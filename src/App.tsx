2d594c8925980a761ea65c92c6ed16bfebb68143
Perttu Haliseva <perttu@gmail.com>
2025-04-10 22:05:12 +0300

    Tweak tagline, fix mobile height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-linear-45 from-purple-600 via-purple-400 to-blue-400">
+    <div className="fixed inset-0 flex items-center justify-center overflow-hidden bg-linear-45 from-purple-600 via-purple-400 to-blue-400">
       <div className="text-center">
         <h1 className="mb-4 font-grandmaster text-6xl text-white tracking-widest drop-shadow-[0_4px_8px_rgba(88,28,135,0.8)] sm:text-7xl md:text-8xl">
           DEV LA SOUL
         </h1>
         <h2 className="px-8 py-2 font-grandmaster text-2xl text-white/90 sm:text-2xl">
           the stakes is high. so is the standard.
+          <br />
+          <br />
+          software development from the heart.
         </h2>
       </div>
     </div>
